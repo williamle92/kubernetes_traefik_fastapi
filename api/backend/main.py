14573c1f36328e921e95e1867bb87e4a270f56dc
William Le <williamkle92@gmail.com>
2025-11-06 23:43:50 -0800

    fix: set root_path

--- api/backend/main.py
+++ api/backend/main.py
@@ -3,7 +3,7 @@ from api.backend.routers.users import router as user_router
 from api.backend.routers.auth import router as auth_router
 from api.backend.worker.tasks import add
 
-app: FastAPI = FastAPI(docs_url="/swagger", redoc_url="/")
+app: FastAPI = FastAPI(docs_url="/swagger", redoc_url="/docs", root_path="/v1/api")
 
 
 app.include_router(user_router)
