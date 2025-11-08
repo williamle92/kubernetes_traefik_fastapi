047ac9bd8569e9dace14641ce6b2071f296f791c
William Le <williamkle92@gmail.com>
2025-11-08 13:19:52 -0800

    feat: fix math route

--- api/backend/main.py
+++ api/backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from api.backend.routers.users import router as user_router
 from api.backend.routers.auth import router as auth_router
 from api.backend.worker.tasks import add
+from pydantic import BaseModel
 
 app: FastAPI = FastAPI(docs_url="/swagger", redoc_url="/docs", root_path="/v1/api")
 
@@ -10,7 +11,12 @@ app.include_router(user_router)
 app.include_router(auth_router)
 
 
+class MathIn(BaseModel):
+    x: int
+    y: int
+
+
 @app.get("/math")
-async def home():
-    result = add.delay(4, 4)
+async def post_math(request: MathIn):
+    result = add(request.x, request.y)
     return {"math": str(result)}
